6965a8c93dc0378694df4d6c8022c3f9cfc1f6c3
Oliynik Maksym <maksymoliynik@gmail.com>
2023-06-23 16:26:04 +0300

    Added main structure and some pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
+import { WorksComponent } from './pages/works/works.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'header', pathMatch: 'full' },
   { path: 'header', component: HeaderComponent},
+  { path: 'works', component: WorksComponent},
+  { path: 'contacts', component: ContactsComponent},
+  { path: 'blog', component: BlogComponent},
 
 ];
 
